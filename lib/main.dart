396449145da035043a2194a4fab76e0791682a10
LinikerThiers <linikerthiers@gmail.com>
2025-07-22 13:05:40 -0300

    conectando o app ao banco de dados

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:listadetarefasestado_app/my_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
